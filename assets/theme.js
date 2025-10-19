01bc491bde15794e1735103a7a66782470d81651
mehdi-touzi <eyeliftz@gmail.com>
2025-10-19 03:02:51 +0200

    Create Denti Luxe Shopify theme

---
+++ assets/theme.js
@@ -0,0 +1,19 @@
+(function () {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('is-visible');
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    {
+      threshold: 0.2
+    }
+  );
+
+  document.querySelectorAll('[data-scroll="fade"]').forEach((element) => {
+    observer.observe(element);
+  });
+})();
